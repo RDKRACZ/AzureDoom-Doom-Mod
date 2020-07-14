1ac3d2124e4a4f0396c0cd4365aa6522ea4e857e
AzureZhen <rcraft@cyberconnects2.com>
2020-07-14 02:54:21 -0400

    Argent Sword isn't stackable anymore.

--- src/main/java/mod/azure/doomweapon/item/weapons/ArgentSword.java
+++ src/main/java/mod/azure/doomweapon/item/weapons/ArgentSword.java
@@ -17,7 +17,7 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class ArgentSword extends SwordItem {
 
 	public ArgentSword() {
-		super(DoomTier.DOOM, 6, -2.4F, new Item.Properties().group(DoomMod.DoomItemGroup));
+		super(DoomTier.DOOM, 6, -2.4F, new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1));
 	}
 
 	@Override
