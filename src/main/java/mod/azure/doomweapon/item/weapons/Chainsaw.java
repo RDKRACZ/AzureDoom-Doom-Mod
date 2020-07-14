55ef774cc56b1b25fb003009c20f4987b78a4338
AzureZhen <rcraft@cyberconnects2.com>
2020-07-14 02:54:08 -0400

    Chainsaw started but not sure what I'm going to do with it.

---
+++ src/main/java/mod/azure/doomweapon/item/weapons/Chainsaw.java
@@ -0,0 +1,14 @@
+package mod.azure.doomweapon.item.weapons;
+
+import mod.azure.doomweapon.DoomMod;
+import mod.azure.doomweapon.util.enums.DoomTier;
+import net.minecraft.item.Item;
+import net.minecraft.item.SwordItem;
+
+public class Chainsaw extends SwordItem {
+
+	public Chainsaw() {
+		super(DoomTier.DOOM, 4, -2.4F, new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1));
+	}
+
+}
