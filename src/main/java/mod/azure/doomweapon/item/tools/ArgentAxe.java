a096ec02c8c4491c0cef322672c763e9c77a9f06
AzureZhen <rcraft@cyberconnects2.com>
2020-07-15 02:36:49 -0400

    Fixed Argent Axe and made Paxel also strip wood

--- src/main/java/mod/azure/doomweapon/item/tools/ArgentAxe.java
+++ src/main/java/mod/azure/doomweapon/item/tools/ArgentAxe.java
@@ -1,23 +1,86 @@
 package mod.azure.doomweapon.item.tools;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.ImmutableMap.Builder;
+import com.google.common.collect.Sets;
 
 import mod.azure.doomweapon.DoomMod;
 import mod.azure.doomweapon.util.enums.DoomTier;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.RotatedPillarBlock;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
-import net.minecraft.item.AxeItem;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.ToolItem;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class ArgentAxe extends AxeItem {
+public class ArgentAxe extends ToolItem {
+
+	private static final Set<Material> field_234662_c_ = Sets.newHashSet(Material.WOOD, Material.field_237214_y_,
+			Material.PLANTS, Material.TALL_PLANTS, Material.BAMBOO, Material.GOURD);
+	private static final Set<Block> field_150917_d_ = Sets.newHashSet(Blocks.LADDER, Blocks.SCAFFOLDING,
+			Blocks.OAK_BUTTON, Blocks.SPRUCE_BUTTON, Blocks.BIRCH_BUTTON, Blocks.JUNGLE_BUTTON, Blocks.DARK_OAK_BUTTON,
+			Blocks.ACACIA_BUTTON, Blocks.field_235358_mQ_, Blocks.field_235359_mR_);
+	protected static final Map<Block, Block> BLOCK_STRIPPING_MAP = (new Builder<Block, Block>())
+			.put(Blocks.OAK_WOOD, Blocks.STRIPPED_OAK_WOOD).put(Blocks.OAK_LOG, Blocks.STRIPPED_OAK_LOG)
+			.put(Blocks.DARK_OAK_WOOD, Blocks.STRIPPED_DARK_OAK_WOOD)
+			.put(Blocks.DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG).put(Blocks.ACACIA_WOOD, Blocks.STRIPPED_ACACIA_WOOD)
+			.put(Blocks.ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG).put(Blocks.BIRCH_WOOD, Blocks.STRIPPED_BIRCH_WOOD)
+			.put(Blocks.BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG).put(Blocks.JUNGLE_WOOD, Blocks.STRIPPED_JUNGLE_WOOD)
+			.put(Blocks.JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG).put(Blocks.SPRUCE_WOOD, Blocks.STRIPPED_SPRUCE_WOOD)
+			.put(Blocks.SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG).put(Blocks.field_235368_mh_, Blocks.field_235369_mi_)
+			.put(Blocks.field_235370_mj_, Blocks.field_235371_mk_).put(Blocks.field_235377_mq_, Blocks.field_235378_mr_)
+			.put(Blocks.field_235379_ms_, Blocks.field_235380_mt_).build();
 
 	public ArgentAxe() {
-		super(DoomTier.DOOM, 8, -2.4F, new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1));
+		super(-2.4F, 8, DoomTier.DOOM, field_150917_d_,
+				new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1));
+	}
+
+	public float getDestroySpeed(ItemStack stack, BlockState state) {
+		Material material = state.getMaterial();
+		return field_234662_c_.contains(material) ? this.efficiency : super.getDestroySpeed(stack, state);
+	}
+
+	public ActionResultType onItemUse(ItemUseContext context) {
+		World world = context.getWorld();
+		BlockPos blockpos = context.getPos();
+		BlockState blockstate = world.getBlockState(blockpos);
+		Block block = BLOCK_STRIPPING_MAP.get(blockstate.getBlock());
+		if (block != null) {
+			PlayerEntity playerentity = context.getPlayer();
+			world.playSound(playerentity, blockpos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+			if (!world.isRemote) {
+				world.setBlockState(blockpos,
+						block.getDefaultState().with(RotatedPillarBlock.AXIS, blockstate.get(RotatedPillarBlock.AXIS)),
+						11);
+				if (playerentity != null) {
+					context.getItem().damageItem(1, playerentity, (p_220040_1_) -> {
+						p_220040_1_.sendBreakAnimation(context.getHand());
+					});
+				}
+			}
+
+			return ActionResultType.func_233537_a_(world.isRemote);
+		} else {
+			return ActionResultType.PASS;
+		}
 	}
 
 	@Override
--- src/main/java/mod/azure/doomweapon/item/tools/ArgentPaxel.java
+++ src/main/java/mod/azure/doomweapon/item/tools/ArgentPaxel.java
@@ -1,8 +1,10 @@
 package mod.azure.doomweapon.item.tools;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.Sets;
 
 import mod.azure.doomweapon.DoomMod;
@@ -10,11 +12,18 @@ import mod.azure.doomweapon.util.enums.DoomTier;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.RotatedPillarBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
 import net.minecraft.item.ToolItem;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
@@ -57,6 +66,16 @@ public class ArgentPaxel extends ToolItem {
 			Blocks.LIME_SHULKER_BOX, Blocks.MAGENTA_SHULKER_BOX, Blocks.ORANGE_SHULKER_BOX, Blocks.PINK_SHULKER_BOX,
 			Blocks.PURPLE_SHULKER_BOX, Blocks.RED_SHULKER_BOX, Blocks.WHITE_SHULKER_BOX, Blocks.YELLOW_SHULKER_BOX,
 			Blocks.PISTON, Blocks.STICKY_PISTON, Blocks.PISTON_HEAD);
+	protected static final Map<Block, Block> BLOCK_STRIPPING_MAP = (new Builder<Block, Block>())
+			.put(Blocks.OAK_WOOD, Blocks.STRIPPED_OAK_WOOD).put(Blocks.OAK_LOG, Blocks.STRIPPED_OAK_LOG)
+			.put(Blocks.DARK_OAK_WOOD, Blocks.STRIPPED_DARK_OAK_WOOD)
+			.put(Blocks.DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG).put(Blocks.ACACIA_WOOD, Blocks.STRIPPED_ACACIA_WOOD)
+			.put(Blocks.ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG).put(Blocks.BIRCH_WOOD, Blocks.STRIPPED_BIRCH_WOOD)
+			.put(Blocks.BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG).put(Blocks.JUNGLE_WOOD, Blocks.STRIPPED_JUNGLE_WOOD)
+			.put(Blocks.JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG).put(Blocks.SPRUCE_WOOD, Blocks.STRIPPED_SPRUCE_WOOD)
+			.put(Blocks.SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG).put(Blocks.field_235368_mh_, Blocks.field_235369_mi_)
+			.put(Blocks.field_235370_mj_, Blocks.field_235371_mk_).put(Blocks.field_235377_mq_, Blocks.field_235378_mr_)
+			.put(Blocks.field_235379_ms_, Blocks.field_235380_mt_).build();
 
 	public ArgentPaxel() {
 		super(8, -2.4F, DoomTier.DOOM, effectiveBlocks,
@@ -67,6 +86,31 @@ public class ArgentPaxel extends ToolItem {
 	public float getDestroySpeed(ItemStack stack, BlockState state) {
 		Material material = state.getMaterial();
 		return field_234662_c_.contains(material) ? this.efficiency : super.getDestroySpeed(stack, state);
+	}
+
+	public ActionResultType onItemUse(ItemUseContext context) {
+		World world = context.getWorld();
+		BlockPos blockpos = context.getPos();
+		BlockState blockstate = world.getBlockState(blockpos);
+		Block block = BLOCK_STRIPPING_MAP.get(blockstate.getBlock());
+		if (block != null) {
+			PlayerEntity playerentity = context.getPlayer();
+			world.playSound(playerentity, blockpos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+			if (!world.isRemote) {
+				world.setBlockState(blockpos,
+						block.getDefaultState().with(RotatedPillarBlock.AXIS, blockstate.get(RotatedPillarBlock.AXIS)),
+						11);
+				if (playerentity != null) {
+					context.getItem().damageItem(1, playerentity, (p_220040_1_) -> {
+						p_220040_1_.sendBreakAnimation(context.getHand());
+					});
+				}
+			}
+
+			return ActionResultType.func_233537_a_(world.isRemote);
+		} else {
+			return ActionResultType.PASS;
+		}
 	}
 
 	@Override
