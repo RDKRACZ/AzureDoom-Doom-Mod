8303fdcc099944aa21ff7c8736fd7086d9976935
AzureZhen <rcraft@cyberconnects2.com>
2020-07-18 12:03:26 -0400

    Soul Cube is now a 5 time use item for saving yourself from death.

--- src/main/java/mod/azure/doomweapon/item/powerup/SoulCubeItem.java
+++ src/main/java/mod/azure/doomweapon/item/powerup/SoulCubeItem.java
@@ -1,52 +1,11 @@
 package mod.azure.doomweapon.item.powerup;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import mod.azure.doomweapon.DoomMod;
-import mod.azure.doomweapon.util.registry.DoomItems;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.Direction;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.LazyOptional;
-import top.theillusivec4.curios.api.CuriosApi;
-import top.theillusivec4.curios.api.CuriosCapability;
-import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class SoulCubeItem extends Item {
 
 	public SoulCubeItem() {
-		super(new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1));
-	}
-
-	@Override
-	public ICapabilityProvider initCapabilities(final ItemStack stack, CompoundNBT unused) {
-		ICurio curio = new ICurio() {
-			@Override
-			public boolean canRightClickEquip() {
-				return true;
-			}
-
-			@Override
-			public boolean canEquip(String identifier, LivingEntity entityLivingBase) {
-				return !CuriosApi.getCuriosHelper().findEquippedCurio(DoomItems.SOULCUBE.get(), entityLivingBase)
-						.isPresent();
-			}
-		};
-
-		return new ICapabilityProvider() {
-			private final LazyOptional<ICurio> curioOpt = LazyOptional.of(() -> curio);
-
-			@Nonnull
-			@Override
-			public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-
-				return CuriosCapability.ITEM.orEmpty(cap, curioOpt);
-			}
-		};
+		super(new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1).maxDamage(5));
 	}
 }
