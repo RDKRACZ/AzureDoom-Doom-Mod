9a4121068e66e6987eb67c2f691a4b6dd8a06f4a
AzureZhen <rcraft@cyberconnects2.com>
2020-07-18 12:08:37 -0400

    Backported Soul Cube Changes

--- src/main/java/mod/azure/doomweapon/DoomMod.java
+++ src/main/java/mod/azure/doomweapon/DoomMod.java
@@ -1,21 +1,40 @@
 package mod.azure.doomweapon;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 import mod.azure.doomweapon.util.LootHandler;
 import mod.azure.doomweapon.util.registry.DoomItems;
 import mod.azure.doomweapon.util.registry.ModEntityTypes;
 import mod.azure.doomweapon.util.registry.ModSoundEvents;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.Direction;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import top.theillusivec4.curios.api.CuriosAPI;
+import top.theillusivec4.curios.api.capability.CuriosCapability;
+import top.theillusivec4.curios.api.capability.ICurio;
 import top.theillusivec4.curios.api.imc.CurioIMCMessage;
 
 @Mod(DoomMod.MODID)
@@ -53,4 +72,65 @@ public class DoomMod {
 			return true;
 		}
 	}).setBackgroundImageName("item_search.png");
+
+	@SubscribeEvent
+	public void attachCapabilities(AttachCapabilitiesEvent<ItemStack> evt) {
+		if (evt.getObject().getItem() != DoomItems.SOULCUBE.get()) {
+			return;
+		}
+		ICurio curio = new ICurio() {
+			@Override
+			public boolean canRightClickEquip() {
+				return true;
+			}
+		};
+		ICapabilityProvider provider = new ICapabilityProvider() {
+			private final LazyOptional<ICurio> curioOpt = LazyOptional.of(() -> curio);
+
+			@Nonnull
+			@Override
+			public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+				return CuriosCapability.ITEM.orEmpty(cap, curioOpt);
+			}
+		};
+		evt.addCapability(CuriosCapability.ID_ITEM, provider);
+	}
+
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public void onLivingDeath(LivingDeathEvent evt) {
+		if (soulCube(evt.getEntityLiving(), evt.getSource())) {
+			evt.setCanceled(true);
+		}
+	}
+
+	private boolean soulCube(LivingEntity livingEntity, DamageSource source) {
+		if (source.canHarmInCreative()) {
+			return false;
+		}
+
+		for (ItemStack held : livingEntity.getHeldEquipment()) {
+			if (held.getItem() == DoomItems.SOULCUBE.get()) {
+				return false;
+			}
+		}
+		return CuriosAPI.getCurioEquipped(DoomItems.SOULCUBE.get(), livingEntity).map(soulcube -> {
+			activateSoulCube(livingEntity, soulcube.getRight());
+			return true;
+		}).orElse(false);
+	}
+
+	private void activateSoulCube(LivingEntity livingEntity, ItemStack soulcube) {
+		ItemStack copy = soulcube.copy();
+		soulcube.damageItem(1, livingEntity, (entity) -> {
+			entity.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+		});
+
+		if (livingEntity instanceof ServerPlayerEntity) {
+			ServerPlayerEntity serverPlayer = (ServerPlayerEntity) livingEntity;
+			serverPlayer.addStat(Stats.ITEM_USED.get(DoomItems.SOULCUBE.get()));
+			CriteriaTriggers.USED_TOTEM.trigger(serverPlayer, copy);
+		}
+		livingEntity.setHealth(20.0F);
+		livingEntity.world.setEntityState(livingEntity, (byte) 35);
+	}
 }
--- src/main/java/mod/azure/doomweapon/item/powerup/SoulCubeItem.java
+++ src/main/java/mod/azure/doomweapon/item/powerup/SoulCubeItem.java
@@ -1,51 +1,11 @@
 package mod.azure.doomweapon.item.powerup;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import mod.azure.doomweapon.DoomMod;
-import mod.azure.doomweapon.util.registry.DoomItems;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.util.Direction;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-import net.minecraftforge.common.util.LazyOptional;
-import top.theillusivec4.curios.api.CuriosAPI;
-import top.theillusivec4.curios.api.capability.CuriosCapability;
-import top.theillusivec4.curios.api.capability.ICurio;
 
 public class SoulCubeItem extends Item {
 
 	public SoulCubeItem() {
-		super(new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1));
-	}
-
-	@Override
-	public ICapabilityProvider initCapabilities(final ItemStack stack, CompoundNBT unused) {
-		ICurio curio = new ICurio() {
-			@Override
-			public boolean canRightClickEquip() {
-				return true;
-			}
-
-			@Override
-			public boolean canEquip(String identifier, LivingEntity entityLivingBase) {
-				return !CuriosAPI.getCurioEquipped(DoomItems.SOULCUBE.get(), entityLivingBase).isPresent();
-			}
-		};
-
-		return new ICapabilityProvider() {
-			private final LazyOptional<ICurio> curioOpt = LazyOptional.of(() -> curio);
-
-			@Nonnull
-			@Override
-			public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
-
-				return CuriosCapability.ITEM.orEmpty(cap, curioOpt);
-			}
-		};
+		super(new Item.Properties().group(DoomMod.DoomItemGroup).maxStackSize(1).maxDamage(5));
 	}
 }
