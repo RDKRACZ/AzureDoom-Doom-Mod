3d6e6b686071c7f2a8edcf9593eb2637a4f4ab3f
AzureZhen <rcraft@cyberconnects2.com>
2020-07-20 03:12:56 -0400

    Fixes drops.

--- src/main/java/mod/azure/doomweapon/entity/PainEntity.java
+++ src/main/java/mod/azure/doomweapon/entity/PainEntity.java
@@ -3,7 +3,6 @@ package mod.azure.doomweapon.entity;
 import java.util.EnumSet;
 import java.util.Random;
 
-import mod.azure.doomweapon.util.registry.DoomItems;
 import mod.azure.doomweapon.util.registry.ModSoundEvents;
 import net.minecraft.entity.CreatureAttribute;
 import net.minecraft.entity.EntitySize;
@@ -18,7 +17,6 @@ import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.controller.MovementController;
 import net.minecraft.entity.ai.goal.Goal;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
-import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.monster.IMob;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FireballEntity;
@@ -211,15 +209,6 @@ public class PainEntity extends FlyingEntity implements IMob {
 	@Override
 	protected float getStandingEyeHeight(Pose poseIn, EntitySize sizeIn) {
 		return 1.0F;
-	}
-
-	protected void dropSpecialItems(DamageSource source, int looting, boolean recentlyHitIn) {
-		super.dropSpecialItems(source, looting, recentlyHitIn);
-		ItemEntity itementity = this.entityDropItem(DoomItems.ARGENT_ENERGY.get());
-		if (itementity != null) {
-			itementity.setNoDespawn();
-			itementity.setGlowing(true);
-		}
 	}
 
 	@Override
