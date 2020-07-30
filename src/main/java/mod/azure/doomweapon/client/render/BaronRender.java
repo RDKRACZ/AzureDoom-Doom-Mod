f96328cc9e3b2343d5824432eee405a16494c5ed
AzureZhen <rcraft@cyberconnects2.com>
2020-07-30 12:15:48 -0400

    Horns on the Baron!

--- src/main/java/mod/azure/doomweapon/client/render/BaronRender.java
+++ src/main/java/mod/azure/doomweapon/client/render/BaronRender.java
@@ -11,19 +11,19 @@ import net.minecraft.util.ResourceLocation;
 
 public class BaronRender extends MobRenderer<BaronEntity, BaronModel<BaronEntity>> {
 
-	protected static final ResourceLocation TEXTURE = new ResourceLocation(DoomMod.MODID, "textures/entity/barron-of-hell.png");
+	protected static final ResourceLocation TEXTURE = new ResourceLocation(DoomMod.MODID,
+			"textures/entity/barron-of-hell_new.png");
 
 	public BaronRender(EntityRendererManager renderManagerIn) {
-		super(renderManagerIn, new BaronModel<BaronEntity>(0.5f, false), 0.5f);
+		super(renderManagerIn, new BaronModel<>(), 0.5f);
 	}
 
 	@Override
 	public ResourceLocation getEntityTexture(BaronEntity entity) {
 		return TEXTURE;
 	}
-	
-	protected void preRenderCallback(BaronEntity entitylivingbaseIn, MatrixStack matrixStackIn,
-			float partialTickTime) {
+
+	protected void preRenderCallback(BaronEntity entitylivingbaseIn, MatrixStack matrixStackIn, float partialTickTime) {
 		matrixStackIn.scale(2.0F, 2.0F, 2.0F);
 	}
 
