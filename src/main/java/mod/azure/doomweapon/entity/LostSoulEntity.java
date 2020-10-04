043949dc1b62d6518e2bd2a52e66fe9b9b046f2c
AzureZhen <rcraft@cyberconnects2.com>
2020-10-04 19:15:45 -0400

    v3.2.1

--- src/main/java/mod/azure/doomweapon/entity/LostSoulEntity.java
+++ src/main/java/mod/azure/doomweapon/entity/LostSoulEntity.java
@@ -3,6 +3,8 @@ package mod.azure.doomweapon.entity;
 import java.util.EnumSet;
 import java.util.Random;
 
+import javax.annotation.Nullable;
+
 import mod.azure.doomweapon.util.registry.ModEntityTypes;
 import mod.azure.doomweapon.util.registry.ModSoundEvents;
 import net.minecraft.block.BlockState;
@@ -35,6 +37,8 @@ public class LostSoulEntity extends DemonEntity implements IMob {
 
 	public int explosionPower = 1;
 	public int flameTimer;
+	@Nullable
+	private BlockPos boundOrigin;
 
 	public LostSoulEntity(EntityType<? extends LostSoulEntity> type, World world) {
 		super(type, world);
@@ -104,6 +108,7 @@ public class LostSoulEntity extends DemonEntity implements IMob {
 	protected void registerGoals() {
 		this.goalSelector.addGoal(8, new LostSoulEntity.LookAroundGoal(this));
 		this.goalSelector.addGoal(4, new LostSoulEntity.ChargeAttackGoal());
+		this.goalSelector.addGoal(8, new LostSoulEntity.MoveRandomGoal());
 		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
 	}
 
@@ -117,6 +122,46 @@ public class LostSoulEntity extends DemonEntity implements IMob {
 	public void livingTick() {
 		super.livingTick();
 		flameTimer = (flameTimer + 1) % 8;
+	}
+
+	@Nullable
+	public BlockPos getBoundOrigin() {
+		return this.boundOrigin;
+	}
+
+	class MoveRandomGoal extends Goal {
+		public MoveRandomGoal() {
+			this.setMutexFlags(EnumSet.of(Goal.Flag.MOVE));
+		}
+
+		public boolean shouldExecute() {
+			return !LostSoulEntity.this.getMoveHelper().isUpdating() && LostSoulEntity.this.rand.nextInt(7) == 0;
+		}
+
+		public boolean shouldContinueExecuting() {
+			return false;
+		}
+
+		public void tick() {
+			BlockPos blockpos = LostSoulEntity.this.getBoundOrigin();
+			if (blockpos == null) {
+				blockpos = LostSoulEntity.this.getPosition();
+			}
+			for (int i = 0; i < 3; ++i) {
+				BlockPos blockpos1 = blockpos.add(LostSoulEntity.this.rand.nextInt(15) - 7,
+						LostSoulEntity.this.rand.nextInt(11) - 5, LostSoulEntity.this.rand.nextInt(15) - 7);
+				if (LostSoulEntity.this.world.isAirBlock(blockpos1)) {
+					LostSoulEntity.this.moveController.setMoveTo((double) blockpos1.getX() + 0.5D,
+							(double) blockpos1.getY() + 0.5D, (double) blockpos1.getZ() + 0.5D, 0.25D);
+					if (LostSoulEntity.this.getAttackTarget() == null) {
+						LostSoulEntity.this.getLookController().setLookPosition((double) blockpos1.getX() + 0.5D,
+								(double) blockpos1.getY() + 0.5D, (double) blockpos1.getZ() + 0.5D, 180.0F, 20.0F);
+					}
+					break;
+				}
+			}
+
+		}
 	}
 
 	@Override
