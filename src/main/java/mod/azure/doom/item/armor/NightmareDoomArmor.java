c3a7c7876d32bbc087b37ff3493cb35b33fe9c35
AzureZhen <rcraft@cyberconnects2.com>
2020-12-01 11:04:36 -0500

    v3.5.0

--- src/main/java/mod/azure/doom/item/armor/NightmareDoomArmor.java
+++ src/main/java/mod/azure/doom/item/armor/NightmareDoomArmor.java
@@ -3,24 +3,58 @@ package mod.azure.doom.item.armor;
 import java.util.List;
 
 import mod.azure.doom.DoomMod;
-import mod.azure.doom.item.armor.skin.SkinArmor;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ArmorStandEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import software.bernie.geckolib3.core.IAnimatable;
+import software.bernie.geckolib3.core.PlayState;
+import software.bernie.geckolib3.core.builder.AnimationBuilder;
+import software.bernie.geckolib3.core.controller.AnimationController;
+import software.bernie.geckolib3.core.event.predicate.AnimationEvent;
+import software.bernie.geckolib3.core.manager.AnimationData;
+import software.bernie.geckolib3.core.manager.AnimationFactory;
+import software.bernie.geckolib3.item.GeoArmorItem;
 
-public class NightmareDoomArmor extends SkinArmor {
+public class NightmareDoomArmor extends GeoArmorItem implements IAnimatable {
+
+	private AnimationFactory factory = new AnimationFactory(this);
+
+	@Override
+	public void registerControllers(AnimationData data) {
+		data.addAnimationController(
+				new AnimationController<NightmareDoomArmor>(this, "controller", 20, this::predicate));
+	}
+
+	private <P extends IAnimatable> PlayState predicate(AnimationEvent<P> event) {
+		LivingEntity livingEntity = event.getExtraDataOfType(LivingEntity.class).get(0);
+		if (livingEntity instanceof ArmorStandEntity) {
+			return PlayState.STOP;
+		}
+		event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
+		return PlayState.CONTINUE;
+	}
+
+	@Override
+	public AnimationFactory getFactory() {
+		return this.factory;
+	}
 
 	public NightmareDoomArmor(IArmorMaterial materialIn, EquipmentSlotType slot) {
 		super(materialIn, slot, new Item.Properties().group(DoomMod.DoomArmorItemGroup).maxStackSize(1));
-
 	}
 
 	@Override
@@ -29,6 +63,32 @@ public class NightmareDoomArmor extends SkinArmor {
 		tooltip.add(new TranslationTextComponent("doom.nightmarearmor.text").applyTextStyle(TextFormatting.YELLOW)
 				.applyTextStyle(TextFormatting.ITALIC));
 		super.addInformation(stack, worldIn, tooltip, flagIn);
+	}
+
+	@Override
+	public boolean shouldCauseReequipAnimation(ItemStack oldStack, ItemStack newStack, boolean slotChanged) {
+		return false;
+	}
+
+	@Override
+	public boolean hasEffect(ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public void fillItemGroup(ItemGroup group, NonNullList<ItemStack> items) {
+		ItemStack stack = new ItemStack(this);
+		stack.hasTag();
+		stack.addEnchantment(Enchantments.FIRE_PROTECTION, 1);
+		if (group == DoomMod.DoomArmorItemGroup) {
+			items.add(stack);
+		}
+	}
+
+	@Override
+	public void onCreated(ItemStack stack, World worldIn, PlayerEntity playerIn) {
+		stack.hasTag();
+		stack.addEnchantment(Enchantments.FIRE_PROTECTION, 1);
 	}
 
 }
